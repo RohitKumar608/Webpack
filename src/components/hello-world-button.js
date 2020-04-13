e1934eec37e190df6e4c9ba7331c8f501168214f
rohitkumarm <rohitkumarrk13568@gmail.com>
2020-04-13 21:07:48 +0530

    Hnadling scss

--- src/components/hello-world-button.js
+++ src/components/hello-world-button.js
@@ -1,17 +1,17 @@
-import './hello-world-button.css';
+import './hello-world-button.scss';
 
 class HelloWorldButton {
     render() {
         const button = document.createElement('button');
-        button.innerHTML = 'Hello world';
-        button.classList.add('hello-world-button');
         const body = document.querySelector('body');
+        button.innerHTML = 'Hello world';
         button.onclick = function () {
             const p = document.createElement('p');
             p.innerHTML = 'Hello world';
             p.classList.add('hello-world-text');
             body.appendChild(p);
         }
+        button.classList.add('hello-world-button');
         body.appendChild(button);
     }
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,33 @@
 const path = require('path');
 
-module.exports ={
+module.exports = {
     entry: './src/index.js',
     output: {
         filename: 'bundle.js',
-        path: path.resolve(__dirname,'./dist'),
-        publicPath: 'dist/',
+        path: path.resolve(__dirname, './dist'),
+        publicPath: 'dist/'
     },
     mode: 'none',
-    module:{
-        rules:[
+    module: {
+        rules: [
             {
                 test: /\.(png|jpg)$/,
-                use:[
+                use: [
                     'file-loader'
                 ]
             },
             {
                 test: /\.css$/,
-                use:[
-                    'style-loader','css-loader'
+                use: [
+                    'style-loader', 'css-loader'
                 ]
             },
+            {
+                test: /\.scss$/,
+                use: [
+                    'style-loader', 'css-loader', 'sass-loader'
+                ]
+            }
         ]
     }
-}
+};
