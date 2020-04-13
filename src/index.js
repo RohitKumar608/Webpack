0eda14bcc0443cc74733bd53cecb79271e38c63c
rohitkumarm <rohitkumarrk13568@gmail.com>
2020-04-13 20:59:20 +0530

    css loder added to load css files

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
-import helloWorld from './hello-world';
-import addImage from './add-image';
-addImage();
-helloWorld();
+import HelloWorld from './components/hello-world-button';
+
+const helloWorld = new HelloWorld();
+helloWorld.render();
+// import addImage from './add-image';
+// addImage();
+// helloWorld();
--- webpack.config.js
+++ webpack.config.js
@@ -15,7 +15,13 @@ module.exports ={
                 use:[
                     'file-loader'
                 ]
-            }
+            },
+            {
+                test: /\.css$/,
+                use:[
+                    'style-loader','css-loader'
+                ]
+            },
         ]
     }
 }
